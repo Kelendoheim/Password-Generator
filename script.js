1f4e3bf920081c8c48f940493474f37ae545d900
TDWalsh <thomas.walsh736@gmail.com>
2020-09-03 21:18:35 -0400

    attempting to fix a discrepency between personal machine and github link

--- script.js
+++ script.js
@@ -97,59 +97,71 @@ var specialCharArray = [
 var indexPosition = 0;
 var finalArray = [];
 var finalPassword = "";
-function generatePassword () {
-
-//prompts user for number between 8 and 128 until correct number is input
-var passwordLength = prompt("Enter desired number of characters for password:");
-
-while (passwordLength < 8 || passwordLength > 128){
-
-  alert("For maximum security, please choose a numeric length between 8 and 128 characters");
-  var passwordLength = prompt("Enter desired number of characters for password:");
-}
-//checks to see which types of character are desired for the password
-var lowercaseBoolean = confirm("Would you like to include lowercase letters?");
-var uppercaseBoolean = confirm("Would you like to include UPPERCASE letters?");
-var numberBoolean = confirm("Would you like to include numbers?");
-var specialCharBoolean = confirm(
-  "Would you like to include special characters?"
-);
-// If no characters are selected, the user will be alerted to try again.
-if (lowercaseBoolean  === false && uppercaseBoolean === false && numberBoolean === false && specialCharBoolean === false) {
-  alert("You have selected no characters for your password. Please try again.");
-};
-
-// checks the boolean values from the confirms and adds the array elements into the finalArray variable.
-if (lowercaseBoolean === true) {
-  finalArray = finalArray.concat(lowercaseArray);
-}
-if (uppercaseBoolean === true) {
-  finalArray = finalArray.concat(uppercaseArray);
-}
-if (numberBoolean === true) {
-  finalArray = finalArray.concat(numberArray);
-}
-if (specialCharBoolean === true) {
-  finalArray = finalArray.concat(specialCharArray);
-}
-
-//function to pick a random index position within an array
-function randomArrayIndex(x) {
-  var indexPosition = Math.floor((Math.random() * x.length)); 
-  return indexPosition;
- 
- };
-for (i = 0; i < passwordLength; i++) {
-  finalPassword = finalPassword.concat(finalArray[randomArrayIndex(finalArray)]);
-}
-return finalPassword;
-
+function generatePassword() {
   
+  //prompts user for number between 8 and 128 until correct number is input
+  var passwordLength = prompt(
+    "Enter desired number of characters for password:"
+  );
+
+  while (passwordLength < 8 || passwordLength > 128) {
+    alert(
+      "For maximum security, please choose a numeric length between 8 and 128 characters"
+    );
+    var passwordLength = prompt(
+      "Enter desired number of characters for password:"
+    );
+  }
+  //checks to see which types of character are desired for the password
+  var lowercaseBoolean = confirm(
+    "Would you like to include lowercase letters?"
+  );
+  var uppercaseBoolean = confirm(
+    "Would you like to include UPPERCASE letters?"
+  );
+  var numberBoolean = confirm("Would you like to include numbers?");
+  var specialCharBoolean = confirm(
+    "Would you like to include special characters?"
+  );
+  // If no characters are selected, the user will be alerted to try again.
+  if (
+    lowercaseBoolean === false &&
+    uppercaseBoolean === false &&
+    numberBoolean === false &&
+    specialCharBoolean === false
+  ) {
+    alert(
+      "You have selected no characters for your password. Please try again."
+    );
+  }
+
+  // checks the boolean values from the confirms and adds the array elements into the finalArray variable.
+  if (lowercaseBoolean === true) {
+    finalArray = finalArray.concat(lowercaseArray);
+  }
+  if (uppercaseBoolean === true) {
+    finalArray = finalArray.concat(uppercaseArray);
+  }
+  if (numberBoolean === true) {
+    finalArray = finalArray.concat(numberArray);
+  }
+  if (specialCharBoolean === true) {
+    finalArray = finalArray.concat(specialCharArray);
+  }
+
+  //function to pick a random index position within an array
+  function randomArrayIndex(x) {
+    var indexPosition = Math.floor(Math.random() * x.length);
+    return indexPosition;
+  }
+  //Add a character to the finalPassword string until the number of characters equals passwordLength
+  for (i = 0; i < passwordLength; i++) {
+    finalPassword = finalPassword.concat(
+      finalArray[randomArrayIndex(finalArray)]
+    );
+  }
+  return finalPassword;
 }
-
-
-
-
 
 // Write password to the #password input
 function writePassword() {
@@ -162,16 +174,3 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-
-
-
-
-
-
-
-//TODO DELETE
-
-console.log(passwordLength);
-console.log(finalPassword);
-console.log(password);
